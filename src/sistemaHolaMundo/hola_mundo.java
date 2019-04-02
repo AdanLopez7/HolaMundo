774ac0fd360710ec47bfd0d8096d221b339b433c
AdanLopez7 <49066435+AdanLopez7@users.noreply.github.com>
2019-04-02 09:14:38 -0400

    agrege una linea mas

--- src/sistemaHolaMundo/hola_mundo.java
+++ src/sistemaHolaMundo/hola_mundo.java
@@ -1,5 +1,12 @@
 package sistemaHolaMundo;
 
 public class hola_mundo {
+	
+	public static void main(String args [])
+	{
+		System.out.println("hola mundo");
+		System.out.println("como estan");
+		
+	}
 
 }
